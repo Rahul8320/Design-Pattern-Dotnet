3344c3ea57752947730e46b0408b4caac2d71277
Rahul Dey <rahuldey832000@gmail.com>
2026-02-26 07:43:15 +0530

    Added LLM based weather forecast text in forecast display

--- WeatherStation/Services/ForecastDisplay.cs
+++ WeatherStation/Services/ForecastDisplay.cs
@@ -1,3 +1,6 @@
+using System.ClientModel;
+using OpenAI;
+using OpenAI.Chat;
 using WeatherStation.Interfaces;
 using WeatherStation.Models;
 
@@ -18,13 +21,7 @@ public class ForecastDisplay : IObserver, IDisplayElement
 
     public void Display()
     {
-        string message = Weather.Temperature switch
-        {
-            > 80 => "Improve weather on the way",
-            > 75 => "Watch out for cooler, rainy weather",
-            _ => "More of the same",
-        };
-
+        string message = GenerateForecastText(weather: Weather);
         Console.WriteLine($"Forecast: {message}");
     }
 
@@ -33,5 +30,38 @@ public class ForecastDisplay : IObserver, IDisplayElement
         Weather = weather;
 
         Display();
+    }
+
+    private string GenerateForecastText(Weather weather)
+    {
+        try
+        {
+            var client = new ChatClient(
+                model: "liquid/lfm2.5-1.2b",
+                credential: new ApiKeyCredential("Not Required"),
+                options: new OpenAIClientOptions()
+                {
+                    Endpoint = new Uri("http://127.0.0.1:1234/v1")
+                }
+            );
+
+            ChatMessage[] messages = [
+                ChatMessage.CreateSystemMessage("You are a weather forecast assistant. Your task it to defined the forecast text for the user based on the temperature, humidity and pressure value provided to you. Don't repeat the weather data provided to you. Keep this forecast message/text short, one liner and to the point."),
+                ChatMessage.CreateUserMessage($"My current weather details: {weather}. Give me the weather forecast based on this data.")
+            ];
+
+            var completion = client.CompleteChat(messages);
+            return completion.Value.Content[0].Text;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occurred during generating forecast message. Error: {ex.Message}");
+            return Weather.Temperature switch
+            {
+                > 80 => "Improve weather on the way",
+                > 75 => "Watch out for cooler, rainy weather",
+                _ => "More of the same",
+            };
+        }
     }
 }
